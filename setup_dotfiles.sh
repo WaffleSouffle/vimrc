01ff84ef2689e4c2f1275a219d0c1cd074b88c39
WaffleSouffle <WaffleSouffle@users.noreply.github.com>
2017-08-03 16:54:11 +0100

    Improve installing plugins, handle Mac.

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env sh
+onMac=0
+osName=$(uname -s)
+if [ "${osName}" == 'Darwin' ]; then
+  onMac=1
+fi
 
-if [ -z ${BASH_SOURCE} ]; then
-  script_dir=$(readlink -f $(dirname "${0}"))
+_gotReadlink=0
+which readlink >/dev/null
+if [ $? -eq 0 ]; then
+  _gotReadlink=1
+fi
+
+_doreadlink() {
+  if [ ${onMac} -eq 1 ]; then
+    readlink "${1}"
+  else
+    readlink -f "${1}"
+  fi
+}
+
+if [ ${_gotReadlink} -eq 1 ] && [ -z ${BASH_SOURCE} ]; then
+  script_dir=$(_doreadlink $(dirname "${0}"))
 else
   script_dir="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 fi
@@ -11,17 +30,40 @@ home_vimrcpath=~/.vimrc
 actual_home_vimrcpath=$home_vimrcpath
 backup_home_vimrcpath=${home_vimrcpath}.old
 
-which readlink >/dev/null
-if [ $? -eq 0 ]; then
-  actual_home_vimrcpath=$(readlink -f "${actual_home_vimrcpath}")
+if [ ${_gotReadlink}? -eq 1 ]; then
+  actual_home_vimrcpath=$(_doreadlink "${actual_home_vimrcpath}")
 fi
+
+_vundleUp() {
+  if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
+    echo 'Installing Vundle - https://github.com/VundleVim/Vundle.vim'
+    git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+  fi
+
+  echo 'Installing Vim plugins'
+  echo 'set nocompatible              " be iMproved, required
+      filetype off                  " required
+
+      set rtp+=~/.vim/bundle/Vundle.vim
+      call vundle#begin()
+' > "${script_dir}"/vimrctemp
+  grep "Plugin '" "${script_vimrcpath}" >> "${script_dir}"/vimrctemp
+  grep "Plugin '" "${backup_home_vimrcpath}" >> "${script_dir}"/vimrctemp
+  echo '      call vundle#end()
+' >> "${script_dir}"/vimrctemp
+
+  vim +PluginInstall +qall -u "${script_dir}"/vimrctemp
+
+  rm "${script_dir}"/vimrctemp
+}
 
 if [ ! "${actual_home_vimrcpath}" -ef "${script_vimrcpath}" ]; then
   source_line="source ${script_vimrcpath}"
   if [ ! -f "${home_vimrcpath}" -o \( -L "${home_vimrcpath}" -a ! -f "${actual_home_vimrcpath}" \) ]; then
-     echo 'Creating new '"${home_vimrcpath}"' file.'
-     rm "${home_vimrcpath}" 2>/dev/null
-     echo "${source_line}" >"${home_vimrcpath}"
+    _vundleUp
+    echo 'Creating new '"${home_vimrcpath}"' file.'
+    rm "${home_vimrcpath}" 2>/dev/null
+    echo "${source_line}" >"${home_vimrcpath}"
   else
     diff -q "${home_vimrcpath}" "${script_vimrcpath}" >/dev/null
     files_differ=$?
@@ -31,25 +73,7 @@ if [ ! "${actual_home_vimrcpath}" -ef "${script_vimrcpath}" ]; then
         echo 'Backing up .vimrc'
         cp "${home_vimrcpath}" "${backup_home_vimrcpath}"
 
-        if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
-          echo 'Installing Vundle - https://github.com/VundleVim/Vundle.vim'
-          git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
-        fi
-
-        echo 'Installing Vim plugins'
-        echo 'set nocompatible              " be iMproved, required
-      filetype off                  " required
-
-      set rtp+=~/.vim/bundle/Vundle.vim
-      call vundle#begin()
-      ' > "${script_dir}"/vimrctemp
-        grep "Plugin '" "${backup_home_vimrcpath}" >> "${script_dir}"/vimrctemp
-        echo 'call vundle#end()
-      ' >> "${script_dir}"/vimrctemp
-
-        vim +PluginInstall +qall -u "${script_dir}"/vimrctemp
-
-        rm "${script_dir}"/vimrctemp
+        _vundleUp
 
         echo 'Updating .vimrc'
         echo "${source_line}" | cat - "${backup_home_vimrcpath}" > "${actual_home_vimrcpath}"
@@ -66,4 +90,3 @@ if [ ! "${actual_home_vimrcpath}" -ef "${script_vimrcpath}" ]; then
 else
   echo '.vimrc already pointing at this git repository.'
 fi
-
